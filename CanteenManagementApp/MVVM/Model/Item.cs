c698bb74d6fcd9707e3dee547096e39d610c8a49
luuquang <luungocquang2002@gmail.com>
2022-11-25 00:09:20 +0700

    test dislay listview

---
+++ CanteenManagementApp/MVVM/Model/Item.cs
@@ -0,0 +1,19 @@
+﻿using CanteenManagementApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanteenManagementApp.MVVM.Model
+{
+    public class Item: ObservableObject
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Describe { get; set; }
+        public double Amount { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
--- CanteenManagementApp/MVVM/ViewModel/StorageViewModel.cs
+++ CanteenManagementApp/MVVM/ViewModel/StorageViewModel.cs
@@ -18,19 +18,15 @@ namespace CanteenManagementApp.MVVM.ViewModel
         public ICollectionView StorageSourceCollection => StorageItemsCollection.View;
         public StorageViewModel()
         {
-            var _allItems = new ObservableCollection<Book>
+            var _allItems = new ObservableCollection<Item>
             {
-                new Book() { Name = "In Search of Lost Time", Author="Marcel Proust",  ImagePath= "/Images/b1.jpg", PublishedYear = "1987"},
-                new Book() { Name = "Ulysses", Author="James Joyce",  ImagePath= "/Images/b2.jpg", PublishedYear = "1977"},
-                new Book() { Name = "Don Quixote", Author="Miguel de Cervantes",  ImagePath="/Images/b3.jpg", PublishedYear = "1968"},
-                new Book() { Name = "The Great Gatsby", Author="F.Scott Fitzgerald",  ImagePath= "/Images/b4.jpg", PublishedYear = "1987"},
-                new Book() { Name = "Moby Dick", Author="Herman Melville",  ImagePath= "/Images/b5.jpg", PublishedYear = "1986"},
-                new Book() { Name = "War and Peace", Author="Leo Tolstoy",  ImagePath= "/Images/b6.jpg", PublishedYear = "1991"},
-                new Book() { Name = "Hamlet", Author="William Shakespeare",  ImagePath= "/Images/b7.jpg", PublishedYear = "1987"},
-                new Book() { Name = "The Odyssey", Author="Homer",  ImagePath= "/Images/b8.jpg", PublishedYear = "1988"},
-                new Book() { Name = "Madame Bovary", Author="Gustave Flaubert",  ImagePath="/Images/b9.jpg", PublishedYear = "1897"},
-                new Book() { Name = "The Divine Comedy", Author="Dante Alighieri",  ImagePath="/Images/b10.jpg", PublishedYear = "1982"},
-                new Book() { Name = "In Search of Lost Time", Author="Marcel Proust",  ImagePath="/Images/b10.jpg", PublishedYear = "1987"},
+                new Item() { Name = "Gà nướng", Amount = 10, Describe = "Ngon", Id = 10, Price = 120000, ImagePath = "/Images/b1.jpg" },
+                new Item() { Name = "Lòng bò xào gà", Amount = 10, Describe = "Ngon", Id = 11, Price = 120000,  ImagePath = "/Images/b2.jpg" },
+                new Item() { Name = "Lòng heo xào gà", Amount = 10, Describe = "Ngon", Id = 12, Price = 120000 ,  ImagePath = "/Images/b3.jpg"},
+                new Item() { Name = "Lòng xào gà", Amount = 10, Describe = "Ngon", Id = 13, Price = 120000,  ImagePath = "/Images/b4.jpg" },
+                new Item() { Name = "Lòng xào gà", Amount = 10, Describe = "Ngon", Id = 14, Price = 120000,  ImagePath = "/Images/b5.jpg" },
+                new Item() { Name = "Lòng xào gà", Amount = 10, Describe = "Ngon", Id = 15, Price = 120000,  ImagePath = "/Images/b1.jpg" },
+
             };
             StorageItemsCollection = new CollectionViewSource {  Source = _allItems };
         }
