40ee3a98beb21de1507d6bc452075b424a363c04
Thanh Tran <newacc2312@gmail.com>
2022-11-24 17:13:43 +0700

    Update ViewModel

--- CanteenManagementApp/MVVM/ViewModel/StorageViewModel.cs
+++ CanteenManagementApp/MVVM/ViewModel/StorageViewModel.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CanteenManagementApp.Core;
+using System;
 
 namespace CanteenManagementApp.MVVM.ViewModel
 {
-    internal class StorageViewModel
+    public class StorageViewModel : ObservableObject
     {
     }
 }
