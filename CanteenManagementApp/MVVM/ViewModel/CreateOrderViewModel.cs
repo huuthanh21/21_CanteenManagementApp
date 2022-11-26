533708baceb06f786e84b3c9e64d6e7100acab50
Thanh Tran <newacc2312@gmail.com>
2022-11-26 18:28:03 +0700

    Change View structure and implement payment page

--- CanteenManagementApp/MVVM/ViewModel/CreateOrderViewModel.cs
+++ CanteenManagementApp/MVVM/ViewModel/CreateOrderViewModel.cs
@@ -1,16 +1,15 @@
 ﻿using CanteenManagementApp.Core;
+using System;
+using System.Windows.Navigation;
 
 namespace CanteenManagementApp.MVVM.ViewModel
 {
     public class CreateOrderViewModel : ObservableObject
     {
-        public RelayCommand PaymentCommand { get; set; }
-        public RelayCommand ReceiptCommand { get; set; }
-        public RelayCommand ResetCommand { get; set; }
-        public RelayCommand BackCommand { get; set; }
+        public RelayCommand NavigatePaymentPageCommand { get; set; }
+
         public CreateOrderViewModel()
         {
-
         }
     }
 }
