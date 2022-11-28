e3556620e15f4dbe23e04598aded6e5c8a142bb5
Duy Quang <vanduyquang2020@gmail.com>
2022-11-28 18:59:24 +0700

    Add UI elements

---
+++ CanteenManagementApp/MVVM/ViewModel/DailyReportModel.cs
@@ -0,0 +1,9 @@
+﻿using CanteenManagementApp.Core;
+using System;
+
+namespace CanteenManagementApp.MVVM.ViewModel
+{
+    public class DailyReportModel : ObservableObject
+    {
+    }
+}
--- CanteenManagementApp/MVVM/ViewModel/ReportViewModel.cs
+++ CanteenManagementApp/MVVM/ViewModel/ReportViewModel.cs
@@ -1,9 +1,41 @@
 ﻿using CanteenManagementApp.Core;
 using System;
+using System.Windows.Input;
 
 namespace CanteenManagementApp.MVVM.ViewModel
 {
     public class ReportViewModel : ObservableObject
     {
+        public static RelayCommand DailyReportCommand { get; set; }
+        public static RelayCommand ReportCommand { get; set; }
+        public View.DailyReport CreateDailyReport { get; set; }
+        public static View.Report CreateReport { get; set; }
+        public static CustomerViewModel CustomerVM { get; set; }
+        private object _currentPage;
+        public object CurrentPage
+        {
+            get { return _currentPage; }
+            set { _currentPage = value; }
+        }
+
+
+
+        public ReportViewModel()
+        {
+            CreateDailyReport = new View.DailyReport();
+            CreateReport = new View.Report();
+
+            CurrentPage = CreateReport;
+
+            DailyReportCommand = new RelayCommand(o =>
+            {
+                CurrentPage = CreateDailyReport;
+            });
+
+            ReportCommand = new RelayCommand(o =>
+            {
+                CurrentPage = CreateReport;
+            });
+        }
     }
 }
